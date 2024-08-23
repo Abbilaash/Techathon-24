f09f00b3ee95d88b9bc2478a1d0d6aa556201c70
Abbilaash <abbilaashat@gmail.com>
2024-08-23 23:52:02 +0530

    todays work

---
+++ app/auth.py
@@ -0,0 +1,32 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate('techathon24-106b3-firebase-adminsdk-ecjpy-5aa2cc6fa2.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+
+def signup(name, email, password):
+    # Create a new user
+    try:
+        new_city_ref = db.collection("users").document()
+        new_city_ref.set({
+            "name": name,
+            "email": email,
+            "password": password
+        })
+        return 1
+    except:
+        return 0
+
+
+def login(email, password):
+    # Check if the user exists
+    users_ref = db.collection("users")
+    query = users_ref.where("email", "==", email).where("password", "==", password).stream()
+    for doc in query:
+        doc_dict = doc.to_dict()
+        return [1, doc_dict.get("name")]
+    return [0, ""]
--- run.py
+++ run.py
@@ -1,14 +1,62 @@
-from flask import render_template, request, redirect, url_for, Flask
+from flask import render_template, request, redirect, url_for, Flask, flash, session
+from app import bot
+from app import auth
 
 app = Flask(__name__)
+app.secret_key = 'kjhgdfkldhsafksdhfkl'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    if 'email' in session:
+        return render_template('index.html')
+    else:
+        return redirect(url_for('login'))
+    
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+
+
+
+# making the login and signup routes
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        auth_ = auth.login(email, password)      # [1, "Abbilaash"]
+        if auth_[0] == 1:
+            session['name'] = auth_[1]
+            session['email'] = email
+            flash("You have successfully logged in.", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Invalid credentials, please try again.", "danger")
+            return redirect(url_for('login'))
+    else:
+        if 'email' in session:
+            return redirect(url_for('home'))
+        return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        auth.signup(name, email, password)
+    return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    session.pop('name', None)
+    flash("You have been logged out.", "success")
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
